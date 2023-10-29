6065d79b4cd0d7020947b6f0e8d2e9807862b64c
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-10-29 23:33:45 +0330

    js file

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const values = document.getElementById("values");
 const hexInput = document.getElementById("hexInput");
 const copyBtn = document.getElementById("setBtn");
 colorPicker = new iro.ColorPicker(".colorPicker", {
-  borderWidth: 2,
-  borderColor: "#000"
+  borderWidth: 1,
+  borderColor: "#fff"
 });
 copyBtn.addEventListener('click', () => {
   navigator.clipboard.writeText(hexInput.value);
